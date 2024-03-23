36163f6766ac0f1728d36bd8be53b9e7e5af9ca9
Bobla-2 <savva050203@gmail.com>
2024-03-23 19:23:15 +0300

    stable version

--- windows app/main.py
+++ windows app/main.py
@@ -9,17 +9,22 @@ from PySide6.QtCore import QIODevice, QTimer, QObject
 from PySide6.QtGui import QIcon
 from serialLib import seriall
 
+import ctype
+
+import win32con
+import win32api
 
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 #
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+    flag_warning = True
 
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
 
         self.menu = QtWidgets.QMenu(parent)
-        self.Action1 = self.menu.addAction("Action1")
+        self.Action1 = self.menu.addAction("off warning")
         self.Action2 = self.menu.addAction("Action2")
         self.exitAction = self.menu.addAction("EXIT")
 
@@ -33,12 +38,20 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         QtCore.QCoreApplication.exit()
 
     def action1(self):
-        pass
+        if self.flag_warning:
+            self.Action1.setText("on warning")
+            self.flag_warning = False
+        else:
+            self.Action1.setText("off warning")
+            self.flag_warning = True
+
 
     def action2(self):
-        self.showMessage("hui", "sosi hui")
+        if self.flag_warning:
+            self.showMessage("hui", "sosi hui")
 
 class MainClass(QtWidgets.QWidget):
+    volLevelApp = []
     def __init__(self):
         super().__init__()
         self.timer = QTimer()
@@ -51,7 +64,10 @@ class MainClass(QtWidgets.QWidget):
         self.ser = seriall(vid, pid, 115200)
         self.timer.timeout.connect(self.ser.startSerialAutoConnect)
         self.ser.SignalSerialStartOk.connect(self.startMassege)
-        self.ser.SignalReadFinish.connect(lambda data: self.showData(data))
+        self.ser.SignalReadButton.connect(lambda comand: self.keyPleerHandle(comand))
+        self.ser.SignalReadVoluem.connect(lambda comand: self.levelVolHandle(comand))
+
+        self.audioSessions = AudioUtilities.GetAllSessions()
         self.timer.start()
 
     def readINIfile(self):
@@ -67,8 +83,29 @@ class MainClass(QtWidgets.QWidget):
             self.trayIcon.showMessage("ERROR", "ERROR don't search ini.txt or current PID/VID")
             return 99999, 99999
 
+    def keyPleerHandle(self, comand):
+        comand = str(comand)
+        if comand.find('play') != -1:
+            win32api.keybd_event(win32con.VK_MEDIA_PLAY_PAUSE, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_MEDIA_PLAY_PAUSE, 0, win32con.KEYEVENTF_KEYUP, 0)
+        elif comand.find('next') != -1:
+            win32api.keybd_event(win32con.VK_MEDIA_NEXT_TRACK, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_MEDIA_NEXT_TRACK, 0, win32con.KEYEVENTF_KEYUP, 0)
+        elif comand.find('prev') != -1:
+            win32api.keybd_event(win32con.VK_MEDIA_PREV_TRACK, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_MEDIA_PREV_TRACK, 0, win32con.KEYEVENTF_KEYUP, 0)
 
+    def levelVolHandle(self, comand):
+        comand = str(comand)
 
+        for session in self.audioSessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name() == self.volLevelApp[0] + ".exe":
+                print("volume.GetMasterVolume(): %s" % volume.GetMasterVolume())
+                volume.SetMasterVolume(float(self.volLevelApp[2:])/100, None)
+            else:
+                if self.trayIcon.flag_warning:
+                    self.trayIcon.showMessage("ERROR", self.volLevelApp[0] + '.exe not found')
 
     def process_folder(self, folder_path):
         byte_arrays = []
@@ -76,6 +113,9 @@ class MainClass(QtWidgets.QWidget):
         for filename in os.listdir(folder_path):
             # Проверяем, что файл имеет расширение .bmp
             if filename.endswith(".bmp"):
+
+                self.volLevelApp.append(str(filename[3:-4]))
+                print(filename[:-4])
                 # Получаем полный путь к файлу
                 file_path = os.path.join(folder_path, filename)
                 # Преобразуем изображение в байтовый массив и добавляем его в список
@@ -98,7 +138,6 @@ class MainClass(QtWidgets.QWidget):
         self.timer.stop()
         for image_byte in range(self.process_folder(".\\icon")):
             self.ser.writeSerial("SET_ICON:" + str(image_byte))
-
 
 
 
--- windows app/serialLib.py
+++ windows app/serialLib.py
@@ -2,6 +2,16 @@
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice, pyqtSignal, QObject
 
+# PC->ESP
+# SET_ICON:posision:icon     -> num=(int[0:4]) icon=(bite arrey)
+
+
+# PC<-ESP
+# SET_VALUE:num:value   -> num=(int[0:4]) value=(int [0:100])
+# SET_BUTTON:str   -> str=(prev/next/play)
+
+
+
 
 class seriall(QObject):
     productIdentifier = 0
@@ -14,6 +24,8 @@ class seriall(QObject):
     SignalInvalidComand = pyqtSignal()
     SignalReadFinish = pyqtSignal(list)
     SignalReadLine = pyqtSignal(str)
+    SignalReadButton = pyqtSignal(str)
+    SignalReadVoluem = pyqtSignal(str)
 
     def __init__(self, vendorIdentifier_ = 0, productIdentifier_ = 0, BaudRate_ = 0):
         super().__init__()
@@ -49,32 +61,16 @@ class seriall(QObject):
         self.SignalSerialStartOk.emit()
 
     def readInpurAndOutput(self):                        #оброботчик всех приходящих пакетов
-        inputSrt = self.serial.readAll()
+        inputSrt = self.serial.readLine()
         print(inputSrt)
-        try:
-            inputSrt = str(inputSrt, 'utf-8')
-            for char in inputSrt:
-                self.mas_ser.append(char)
-                if '\n' in self.mas_ser:
-                    out_str = ''.join(self.mas_ser[:-1])
-                    print(out_str)
-                    self.SignalReadLine.emit(out_str)
-                    if len(self.mas_iner) > 40:
-                        self.flag_read_data = False
-                        self.mas_iner.clear()
-                    if 'REG_OK' in out_str:
-                        self.SignalSerialRegOk.emit()
-                        if len(self.mas_iner) > 0:
-                            self.flag_read_data = False
-                            self.SignalReadFinish.emit(self.mas_iner)
+        inputSrt = str(inputSrt, 'utf-8')
+        if inputSrt.find("SET_BUTTON:") != -1:
+            self.SignalReadButton.emit(inputSrt[12])
+        elif inputSrt.find("SET_VALUE") != -1:
+            self.SignalReadVoluem.emit(inputSrt[12])
 
-                            self.mas_iner.clear()
-                    else:
-                        if self.flag_read_data:
-                            self.mas_iner.append(out_str)
-                    self.mas_ser.clear()
-        except:
-            pass
+
+
 
 
     def closeSerial(self):                 #закрываем serial и стираем все состояния кнопок в приложении
